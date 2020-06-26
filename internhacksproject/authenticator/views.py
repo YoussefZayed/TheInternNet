fe0b7a27de62f8e117b19c995357ff92a502a7b7
Braulio Carrion Corveira <45177553+Carr-23@users.noreply.github.com>
2020-06-26 14:37:53 -0400

    Login Setup

--- internhacksproject/authenticator/views.py
+++ internhacksproject/authenticator/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+    else:
+        form = UserCreationForm()
 
     context = {'form':form}
     return render (request,'Pages/register.html',context)
 
 def login(request):
-    context = {}
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('https://test.com/')
+    else:
+        form = AuthenticationForm()
+
+    context = {'form':form}
     return render (request,'Pages/login.html',context)
