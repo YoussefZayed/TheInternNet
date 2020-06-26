672d05c4e89da69b1121b4b9a4874ba8fe0c99e0
Braulio Carrion Corveira <45177553+Carr-23@users.noreply.github.com>
2020-06-26 12:59:03 -0400

    Saving New User Information

--- internhacksproject/authenticator/views.py
+++ internhacksproject/authenticator/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def register(request):
     form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     context = {'form':form}
     return render (request,'Pages/register.html',context)
 
