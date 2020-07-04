528bf064aae6a41a66356b3d2735a4ee4abbec97
YoussefZayed <Youssef.ah.Zayed@gmail.com>
2020-07-03 20:26:18 -0400

    Template creation, editing, deletetion created, variable amount is now dynamic when createing Templates

--- internhacksproject/emailing/urls.py
+++ internhacksproject/emailing/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('viewTemp/<int:email_id>/', views.viewEmailTemp, name='viewEmailTemp'),
     path('createEmail/<int:email_id>/', views.usetemp, name='usetemp'),
     path('createdEmail/<int:email_id>/', views.usedtemp, name='usedtemp'),
+    path('createtemplate/', views.createTemp, name='createTemp'),
+    path('edittemplate/<int:email_id>/', views.editTemp, name='editTemp'),
+    path('deletetemplate/<int:email_id>/', views.deleteTemp, name='deleteTemp'),
     path('<int:contact_id>/', views.detail, name='detail'),
-
 ]
--- internhacksproject/emailing/views.py
+++ internhacksproject/emailing/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.template import loader
+import re
 from .models import Contact, Email
 
 def index(request):
@@ -30,4 +31,29 @@ def usedtemp(request, email_id):
 # uses a template, asks user for fields to fill in 
 def usetemp(request, email_id):
     email = get_object_or_404(Email,pk=email_id)
-    return render(request, 'useTemplate.html', {'email': email})
+    variables = re.findall(r'\$+\b[^\d\W]+',email.content)
+
+    return render(request, 'useTemplate.html', {'email': email, 'variables': variables})
+
+def createTemp(request):
+    if request.POST:
+        template = Email(name= request.POST['templateName'],content = request.POST['templateContent'])
+        template.save()
+        return redirect('/emailing/')
+    else:
+        return render(request, 'createTemplate.html')
+
+def editTemp(request, email_id):
+    email = get_object_or_404(Email,pk=email_id)
+    if request.POST:
+        email.name = request.POST['templateName']
+        email.content = request.POST['templateContent']
+        email.save()
+        return redirect('/emailing/')
+    else:
+        return render(request, 'editTemplate.html', {'template': email})
+
+def deleteTemp(request,email_id):
+    email = get_object_or_404(Email,pk=email_id)
+    email.delete()
+    return redirect('/emailing/')
