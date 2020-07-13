b161e76f13d68302804d01085cd978b18a3cb787
dennikey <dennisbae7@gmail.com>
2020-07-12 20:16:28 -0400

    Update views.py

--- internhacksproject/jobportal/views.py
+++ internhacksproject/jobportal/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-import request
+from django.views import View
+
+from .forms import URLForm
+
+from bs4 import BeautifulSoup
+import requests
+
 # Create your views here.
+class Base(View):
+    def get(self, request, *awgs, **kwargs):
+        return render(request, 'base.html')
+
+class WebPortal(View):
+    def get(self, request, *awgs, **kwargs):
+        context = {}
+        context['url_form'] = URLForm()
+        return render(request, 'webportal/form.html', context)
+
+    def post(self, request):
+        submitbutton= request.POST.get('Submit')
+
+        name = ''
+        weblink = ''
+
+        form = URLForm(request.POST or None)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            weblink = form.cleaned_data.get('weblink')
+
+        context= {'url_form': URLForm(), 'name': name, 'weblink': weblink,
+              'submitbutton': submitbutton}
+        
+        return render(request, 'webportal/form.html', context)
