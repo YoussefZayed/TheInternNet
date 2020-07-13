5a671832983f20f81391d8a11bbf90e1f418c404
YoussefZayed <Youssef.ah.Zayed@gmail.com>
2020-07-12 20:37:47 -0400

    postings model created

--- internhacksproject/jobportal/models.py
+++ internhacksproject/jobportal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+#owner = models.ForeignKey(User, on_delete=models.CASCADE)
+class Posting(models.Model):
+    title= models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    level = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    description = models.TextField()
+    dateCreated = models.DateField(auto_now_add=True)
+
+class SavedPosting(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    dateApplied = models.DateField(auto_now_add=True)
+    template = models.ForeignKey(Posting, on_delete=models.CASCADE)
